784a5b7bfd13f779246c42b3d693cf74f515741d
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2023-11-23 21:20:38 +0600

    lesson_06
    lesson_06

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
@@ -15,12 +17,47 @@ class Rubric(models.Model):
         ordering = ['name']
 
 class Bb(models.Model):
+
+    # class Kinds(models.TextChoices):
+    #     BUY = 'b', 'Куплю'
+    #     SELL = 's', 'Продам'
+    #     RENT = 'r'
+    #
+    # kind = models.CharField(max_length=1, choices=Kinds.choices, default=Kinds.SELL)
+
+    # ------------------------------------------------------------------------------------------
+
+    # KINDS = (
+    #     ('Купля-продажа',(
+    #         ('b', 'Куплю'),
+    #         ('s', 'Продам'),
+    #     )),
+    #     ('Обмен', (
+    #         ('c', 'Обменяю'),
+    #      ))
+    # )
+
+    # ------------------------------------------------------------------------------------------
+
+    KINDS = (
+        (None, 'Выберите тип объявления'),
+        ('b', 'Куплю'),
+        ('s', 'Продам'),
+        ('c', 'Обменяю'),
+    )
+
+    # kind = models.CharField(max_length=1, choices=KINDS, default='s')
+    kind = models.CharField(max_length=1, choices=KINDS, blank=True)
+
     rubric = models.ForeignKey("Rubric", null=True, on_delete=models.PROTECT,
                                verbose_name='Рубрика') # foreignKey = нижний ключ
     title = models.CharField(max_length=50, verbose_name="Товар")
     content = models.TextField(null=True, blank=True, verbose_name="Описание")
-    price = models.FloatField(null=True, blank=True, verbose_name="Цена")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Цена", default=0)
+    is_active = models.BooleanField(default=is_active_default)
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Опубликовано")
+    updated = models.DateTimeField(auto_now=True, db_index=True, verbose_name="Опубликовано")
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     # def __str__(self):
     #    pass
