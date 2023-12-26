b6a8bdeb8713e5d99136e7dc5aa09c530dd67292
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2023-12-26 21:12:59 +0600

    lesson_15_files

--- samplesite/testapp/urls.py
+++ samplesite/testapp/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import  admin
-from django.urls import path, include
+from django.urls import path
+
+from .views import index
+
+app_name = 'testapp'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('test/', include('testapp.urls', namespace='test')),
-    path('', include('bboard.urls')),
+    path('', index, name='index')
 ]
