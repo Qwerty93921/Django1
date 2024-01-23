8380bd4b792b564ddf9c20de78abbe5715ccb4e9
Костюшок Артем Анатольевич <@step.kz>
2024-01-23 20:14:12 +0600

    lesson_21_files

--- samplesite/samplesite/settings.py
+++ samplesite/samplesite/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
 
                 # 'django.template.context_processors.csrf',
-                # 'django.template.context_processors.static',
+                'django.template.context_processors.static',
                 # 'django.template.context_processors.media',
             ],
         },
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
